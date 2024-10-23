e913e55bb3420172b94ab166dbc157b7cf8806e9
ifirmawan <firmawaneiwan@gmail.com>
2024-10-23 18:01:46 +0700

    [#3] Add more args in download.sh
    DOWNLOAD_LIST = download logs
    TARGET_DIR = target download directory
    MY_AUTH0_USER = Email credentials for earthdata portal

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,16 +2,18 @@
 
 GES_URL="$1"
 GES_PATTERN="$2"
-DOWNLOAD_LIST="./download.log"
+DOWNLOAD_LIST="$3"
+TARGET_DIR="$4"
+MY_AUTH0_USER="$5"
 
 curl -s "${GES_URL}" \
 	| grep "${GES_PATTERN}" \
 	| pup \
 	| grep -v "href" \
 	| grep "${GES_PATTERN}" \
-	| sed "s/\ //g" > ./download.log
+	| sed "s/\ //g" > "${DOWNLOAD_LIST}"
 
-echo "FILES TOBE DOWNLOADED: $(wc -l ./download.log)"
+echo "FILES TOBE DOWNLOADED: $(wc -l "${DOWNLOAD_LIST}")"
 
 # Read the file line by line
 while IFS= read -r GES_FILE; do
@@ -19,6 +21,6 @@ while IFS= read -r GES_FILE; do
     wget --load-cookies ./.urs_cookies \
 			--keep-session-cookies --user="${MY_AUTH0_USER}" \
 			--content-disposition --content-disposition –r -c -nH -nd -np -A ".nc4" \
-			"${GES_URL}/${GES_FILE}"
+			-P "${TARGET_DIR}" "${GES_URL}/${GES_FILE}"
 done < "${DOWNLOAD_LIST}"
 
