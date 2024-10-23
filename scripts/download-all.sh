f689a3af2e272a42d3da003001a0038d6b4c6ba1
ifirmawan <firmawaneiwan@gmail.com>
2024-10-23 19:09:10 +0700

    [#3] Fix download-all.sh to run in parallel

--- scripts/download-all.sh
+++ scripts/download-all.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-LOG_DIR=~/log/download-log
+LOG_DIR=~/log/
 
 mkdir -p "${LOG_DIR}"
 
 # Define log file
 CURR_DATE=$(date +%Y%m%d)
 CURR_TIME=$(date +%H%M%S)
-
-CHIRPS_LOG_FILE=${LOG_DIR}/chirps-${CURR_DATE}-${CURR_TIME}.log
-SOIL_MOISTURE_LOG_FILE=${LOG_DIR}/soil-moisture-${CURR_DATE}-${CURR_TIME}.log
-NDVI_LOG_FILE=${LOG_DIR}/ndvi-${CURR_DATE}-${CURR_TIME}.log
-LST_LOG_FILE=${LOG_DIR}/lst-${CURR_DATE}-${CURR_TIME}.log
-
-# Define directories
-CHIRPS_DIR=~/dataset/CHIRPS
-SOIL_MOISTURE_DIR=~/dataset/SOIL_MOISTURE
-NDVI_DIR=~/dataset/NDVI
-LST_DIR=~/dataset/LST
 
 EMAIL="$1"
 
@@ -30,41 +19,28 @@ function download_file()
 {
     URL=$1
     PATTERN=$2
-    LOG_FILE=$3
-    TARGET_DIR=$4
-    EMAIL=$5
+    NAME=$3
 
-    mkdir -p "${TARGET_DIR}"
+    mkdir -p ~/dataset/"${NAME}"
 
-    ./download.sh "${URL}" "${PATTERN}" "${LOG_FILE}" "${TARGET_DIR}" "${EMAIL}"
+    ./download.sh "${URL}" "${PATTERN}" "${LOG_DIR}/${NAME}-${CURR_DATE}-${CURR_TIME}.log" ~/dataset/"${NAME}" "${EMAIL}"
 }
 
 # main script
+download_file "https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/tifs/" ".tif.gz" "CHIRPS" &
+download_file "https://droughtcenter.unl.edu/Outgoing/CDI/data/CDI-Input/NDVI/" ".zip" "NDVI" &
+download_file "https://droughtcenter.unl.edu/Outgoing/CDI/data/CDI-Input/LST/" ".zip" "LST" &
+
 {
-    ./login.sh "${EMAIL}"
-
-    download_file "https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/tifs/" ".tif.gz" "${CHIRPS_LOG_FILE}" "${CHIRPS_DIR}" "${EMAIL}"
-
-    download_file "https://droughtcenter.unl.edu/Outgoing/CDI/data/CDI-Input/NDVI/" ".zip" "${NDVI_LOG_FILE}" "${NDVI_DIR}" "${EMAIL}"
-
-    download_file "https://droughtcenter.unl.edu/Outgoing/CDI/data/CDI-Input/LST/" ".zip" "${LST_LOG_FILE}" "${LST_DIR}" "${EMAIL}"
-
     URL="https://hydro1.gesdisc.eosdis.nasa.gov/data/FLDAS/FLDAS_NOAH01_C_GL_M.001/"
     URL_DIRS=$(curl -S "${URL}" \
         | grep "\[DIR\]" \
         | grep -v "doc" \
         | grep -oP '(?<=href=")[^"]*')
 
-
     for URLS in ${URL_DIRS}; do
         GES_URL="${URL}${URLS}"
-
         GES_PATTERN="FLDAS.*\.nc"
-        download_file "${GES_URL}" "${GES_PATTERN}" "${SOIL_MOISTURE_LOG_FILE}" "${SOIL_MOISTURE_DIR}" "${EMAIL}"
+        download_file "${GES_URL}" "${GES_PATTERN}" "SOIL_MOISTURE"
     done
-
-    wait
-    echo "All downloads completed."
 } &
-
-echo "Background process started with PID $!"
