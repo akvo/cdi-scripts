5167265e39cd2a188598d2a21064e90c6578cb3e
ifirmawan <firmawaneiwan@gmail.com>
2024-12-24 09:54:57 +0700

    [#19] Add update metadata: category

--- jobs/upload_cdi_to_geonode_job.py
+++ jobs/upload_cdi_to_geonode_job.py
@@ -9,6 +9,14 @@ password = os.getenv("GEONODE_PASSWORD")
 dataset_path = os.getenv("DATASET_PATH")
 dataset_type = os.getenv("DATASET_TYPE")
 
+# List of cateogries ids in Geonode
+categories = {
+    "cdi": 22,
+    "lst": 23,
+    "ndvi": 24,
+    "spi": 25,
+}
+
 
 def write_failure_message(response):
     print("Request failed.")
@@ -66,7 +74,7 @@ def update_dataset_metadata(dataset_id, metadata):
         write_failure_message(response)
 
 
-def tracking_upload_progress(execution_id):
+def tracking_upload_progress(execution_id: int, taxonomy: str):
     if not execution_id:
         return
     api_url = f"{geonode_url}api/v2/executionrequest/{execution_id}"
@@ -74,7 +82,7 @@ def tracking_upload_progress(execution_id):
     if response.status_code == 200:
         json_response = response.json()["request"]
         if json_response.get("status") != "finished":
-            tracking_upload_progress(execution_id)
+            tracking_upload_progress(execution_id, taxonomy)
         else:
             dataset = json_response.get("output_params").get("resources")[0]
             if dataset.get("id"):
@@ -83,6 +91,7 @@ def tracking_upload_progress(execution_id):
                     {
                         "advertised": False,
                         "is_published": False,
+                        "category": categories.get(taxonomy),
                     },
                 )
     else:
@@ -90,12 +99,31 @@ def tracking_upload_progress(execution_id):
         return None
 
 
+# def create_category(taxonomy):
+#     api_url = f"{geonode_url}api/v2/keywords/"
+#     category_name = f"{taxonomy.lower()}-ratser-map"
+#     category_description = f"{taxonomy} Raster Map"
+#     response = requests.post(
+#         api_url,
+#         auth=(username, password),
+#         json={"name": category_name,  "description": category_description}
+#     )
+#     if response.status_code == 201:
+#         print(f"Category {category_name} created successfully!")
+#     else:
+#         write_failure_message(response)
+
+
 def main():
     dataset_files = get_all_dataset_files()
     for dataset_file in dataset_files:
         print(f"Uploading {dataset_file} to GeoNode...")
         execution_id = upload_to_geonode(dataset_file)
-        tracking_upload_progress(execution_id)
+        taxonomy = dataset_file.split('/')[-2]
+        tracking_upload_progress(
+            execution_id,
+            taxonomy.lower()
+        )
 
 
 if __name__ == '__main__':
