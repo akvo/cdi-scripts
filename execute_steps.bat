3976715cfb076e86e606d7a01d4ed3f5c855c5a0
ifirmawan <firmawaneiwan@gmail.com>
2025-09-05 10:06:52 +0700

    [#29] Remove obsolete execute_steps.bat script

--- execute_steps.bat
+++
@@ -1,9 +0,0 @@
-setlocal
-set PATH=%PATH%;C:\ProgramData\Anaconda3;C:\ProgramData\Anaconda3\scripts
-
-D:
-cd Operations\Global_CDI\SA-eSwatini\scripts
-
-C:\ProgramData\Anaconda3\envs\cdi_37\python.exe STEP_0000_execute_all_steps.py
-
-endlocal
